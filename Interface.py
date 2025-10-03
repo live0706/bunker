97908e9241cbe3ec0f71e5ad95c46a7445a21208
Arsene2005 <arayimbengue9@gmail.com>
2025-10-03 16:28:40 +0000

    deuxieme commit,affichage et fonction principale de l'interface

--- Interface.py
+++ Interface.py
@@ -18,3 +18,65 @@ accounts = {
     str(ross_account.account_number): ross_account,
     str(rachel_account.account_number): rachel_account
 }
+
+# Fenêtre principale (Login)
+root = tk.Tk()
+root.title("Banque - Login")
+root.geometry("400x250")
+root.configure(bg="white")
+
+username_var = tk.StringVar()
+account_id_var = tk.StringVar()
+
+def login():
+    name = username_var.get()
+    acc_id = account_id_var.get()
+
+    if acc_id in accounts and accounts[acc_id].holder_name.lower() == name.lower():
+        messagebox.showinfo("Succès", f"Bienvenue {name} !")
+        open_dashboard(accounts[acc_id])
+    else:
+        messagebox.showerror("Erreur", "Nom ou ID incorrect")
+
+# Dashboard
+def open_dashboard(account: account):
+    dashboard = tk.Toplevel(root)
+    dashboard.title("Tableau de bord bancaire")
+    dashboard.geometry("500x400")
+    dashboard.configure(bg="white")
+
+    # Ajout historique si inexistant
+    if not hasattr(account, "history_tk"):
+        account.history_tk = [(datetime.now(), account.balance)]
+
+ # Nom + ID
+    tk.Label(dashboard, text=f" {account.holder_name} | ID: {account.account_number}",
+             font=("Segoe UI", 12, "bold"), fg="blue", bg="white").pack(pady=5)
+
+    balance_label = tk.Label(dashboard, text=f" Solde actuel : {account.balance} €",
+                             font=("Segoe UI", 14, "bold"), fg="blue", bg="white")
+    balance_label.pack(pady=10)
+
+    # Dépôt
+    tk.Label(dashboard, text="Montant à déposer :", fg="blue", bg="white").pack()
+    deposit_var = tk.DoubleVar()
+    tk.Entry(dashboard, textvariable=deposit_var).pack()
+
+    def do_deposit():
+        account.deposit(deposit_var.get())
+        balance_label.config(text=f"Solde actuel : {account.balance} €")
+        account.history_tk.append((datetime.now(), account.balance))
+
+    tk.Button(dashboard, text="Déposer", command=do_deposit, bg="blue", fg="white").pack(pady=5)
+
+    # Retrait
+    tk.Label(dashboard, text="Montant à retirer :", fg="blue", bg="white").pack()
+    withdraw_var = tk.DoubleVar()
+    tk.Entry(dashboard, textvariable=withdraw_var).pack()
+
+    def do_withdraw():
+        account.withdraw(withdraw_var.get())
+        balance_label.config(text=f" Solde actuel : {account.balance} €")
+        account.history_tk.append((datetime.now(), account.balance))
+
+    tk.Button(dashboard, text="Retirer", command=do_withdraw, bg="blue", fg="white").pack(pady=5)
